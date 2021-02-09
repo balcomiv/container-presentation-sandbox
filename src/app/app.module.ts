70596848ed1223998ffc0185acee5654c062b76d
BalcomIV <35431931+balcomiv@users.noreply.github.com>
2021-02-09 06:05:23 -0500

    update - app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home.component';
-import { NewsletterComponent } from './components/newsletter.component';
-import { ContainerOneComponent } from './components/containers/container-one/container-one.component';
-import { ContainerTwoComponent } from './components/containers/container-two/container-two.component';
-import { PresentationOneComponent } from './components/containers/presentation-one/presentation-one.component';
-import { PresentationTwoComponent } from './components/presentation/presentation-two/presentation-two.component';
-import { PresentationThreeComponent } from './components/presentation/presentation-three/presentation-three.component';
+import { ContainerOneComponent } from './components/containers/container-one.component';
+import { ContainerTwoComponent } from './components/containers/container-two.component';
+import { PresentationOneComponent } from './components/presentation/presentation-one.component';
+import { PresentationThreeComponent } from './components/presentation/presentation-three.component';
+import { PresentationTwoComponent } from './components/presentation/presentation-two.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    NewsletterComponent,
     ContainerOneComponent,
     ContainerTwoComponent,
     PresentationOneComponent,
     PresentationTwoComponent,
-    PresentationThreeComponent
+    PresentationThreeComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
